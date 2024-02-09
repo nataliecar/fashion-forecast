24b9703f761cd5ae5816fff736134fb881efcfb5
Natalie Cardoso <natcar109@gmail.com>
2024-02-09 17:39:53 -0500

    start of instaloader testing

--- instaloader-test.py
+++ instaloader-test.py
@@ -1,2 +1,21 @@
+import numpy as np
+import pandas as pd
 import instaloader
+from instaloader import Profile
 
+# Get instance
+
+L = instaloader.Instaloader()
+L.interactive_login('natalierc_')
+
+#p = L.download_profiles(profile)
+
+profile = Profile.from_username(L.context, 'voguemagazine')
+data = {}
+post_arr = np.array()
+for post in profile.get_posts():
+    post_arr.append(post)
+data[profile.username] = post_arr
+
+print(post_arr[0])
+#df = pd.DataFrame(profile_arr, columns=['profile_name', ])
